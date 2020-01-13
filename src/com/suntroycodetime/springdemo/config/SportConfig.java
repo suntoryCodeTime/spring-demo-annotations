a2786115d4e88041399d8a4d3f17dca8b653e38d
suntoryCodeTime <dldavidley@gmail.com>
2020-01-13 09:15:20 -0700

    Added Spring Configuration with Jaca Code (NO XML)

---
+++ src/com/suntroycodetime/springdemo/config/SportConfig.java
@@ -0,0 +1,9 @@
+package com.suntroycodetime.springdemo.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.suntroycodetime.springdemo")
+public class SportConfig {
+	
+}
