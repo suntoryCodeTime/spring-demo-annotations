d9820340a9197dc0287b21ae710cda9bdb85dc66
suntoryCodeTime <dldavidley@gmail.com>
2020-06-21 17:04:16 -0700

    stubbed implementation to demonstrate use of Qualifer annotaion

---
+++ src/com/suntorycodetime/springdemo/services/RESTFortuneService.java
@@ -0,0 +1,16 @@
+package com.suntorycodetime.springdemo.services;
+
+import org.springframework.stereotype.Component;
+
+import com.suntorycodetime.springdemo.FortuneService;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
